cd7e7269855ed8d9db6eedaca2e28c95a920b934
jitender <cjatinder39@gmail.com>
2024-05-09 11:15:47 +0530

    added loader and redirect after create post

--- frontend/src/components/CreatePost.jsx
+++ frontend/src/components/CreatePost.jsx
@@ -1,5 +1,6 @@
-import { Button, Container, Grid, TextField, Typography } from '@mui/material'
+import { Backdrop, Button, CircularProgress, Container, Grid, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -9,7 +10,14 @@ export default function CreatePost() {
     email:"",
     description:""
   })
+  const navigate = useNavigate();
   const [error, setError] = useState()
+
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const handleChange = (key, value) => {
     setData({
       ...data,
@@ -17,6 +25,7 @@ export default function CreatePost() {
     })
   }
   const submitHandle = async() => {
+    setOpen(true);
     const response = await fetch("http://localhost:5000/api/user", {
       method: "POST",
       body: JSON.stringify(data),
@@ -33,11 +42,25 @@ export default function CreatePost() {
 
     if(response.ok){
       console.log(result);
+      setData({
+        title:"",
+        email:"",
+        description:""
+      })
+      setOpen(false);
+      navigate('/all')
     }
   }
   const notify = () => toast(error);
   return (
     <Container maxWidth="lg">
+       <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={handleClose}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
       <ToastContainer />
       <Typography variant='h4' textAlign={"center"}>Enter the details 👽</Typography>
       <Grid container spacing={2} my={3}>
--- frontend/src/components/ViewPost.jsx
+++ frontend/src/components/ViewPost.jsx
@@ -34,19 +34,14 @@ export default function ViewPost() {
           data?.length == 0 && <Box display={"flex"} justifyContent={"center"} alignItems={"center"} width={"100%"} height={"80vh"} fontSize={"30px"}>Sorry, No Data Found Yet 👽</Box>
         }
         {
-          data.map((item) => {
+          data?.map((item) => {
             return <Grid key={item._id} item md={4}>
               <Card sx={{ minWidth: 275 }}>
                 <CardContent>
                   <Typography sx={{ fontSize: 30 }} color="black"> {item.title} </Typography>
 
-                  <Typography sx={{ fontSize: 10 }} color="text.secondary" gutterBottom>
-                    {item.email}
-                  </Typography>
-
-                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    {item.description}
-                  </Typography>
+                  <Typography sx={{ fontSize: 10 }} color="text.secondary" gutterBottom> {item.email} </Typography>
+                  <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom> {item.description.length > 50 ? item.description.slice(0, 400) : item.description }</Typography>
                 </CardContent>
                 {/* <CardActions>
                   <Button size="small">Learn More</Button>
